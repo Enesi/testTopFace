a868ca0b6f08a82f24dfcbae82a0fec6979bb54b
Enesi <enesi@rambler.ru>
2016-11-04 02:39:58 +0200

    ver3
    +highlight of saved tags
    - highlight in process of typing

--- viewLogic.js
+++ viewLogic.js
@@ -46,10 +46,45 @@ function updateData(tagIndex)
 		console.log("tag="+tag+",index="+tagIndex)
 	for (var i=0; i<allNotes.length;i++)
 	if(!tag || (allNotes[i].tags.indexOf(tag)!=-1))
-		notes.innerHTML="<div id='txtNote"+i+"' class='scroll' >"+ allNotes[i].content + "</div><div id='noteEditDel"+i+"'><button onClick='onEditNoteClick("+i+");'>Править</button><button onClick='onDeleteNoteClick("+i+");'>X</button></div>"
+		notes.innerHTML="<div id='txtNote"+i+"' class='scroll'  >"+ allNotes[i].content + "</div><div id='noteEditDel"+i+"'><button onClick='onEditNoteClick("+i+");'>Править</button><button onClick='onDeleteNoteClick("+i+");'>X</button></div>"
 		+"<div id='noteOkCancel"+i+"' style='display:none;'><button onClick='onSaveNoteClick("+i+");'>Сохранить</button><button id='btnCancel"+i+"' onClick='onCancelNoteClick("+i+");'>Отмена</button><br></div> tags:"+allNotes[i].tags+"<br><br>"+notes.innerHTML;
 	for (var i=0; i<allTags.length;i++)
 		tags.innerHTML="<p id='pTag"+i+"' onclick=updateData("+i+")>"+allTags[i]+" <button onClick='onDeleteTagClick("+i+");'>X</button></p>"+tags.innerHTML;		
+}
+
+var savedRange;
+function saveSelection()
+{
+    if(window.getSelection)//non IE Browsers
+    {
+        savedRange = window.getSelection().getRangeAt(0);
+    }
+    else if(document.selection)//IE
+    { 
+        savedRange = document.selection.createRange();  
+    } 
+}
+
+function restoreSelection(el)
+{
+    el.focus();
+    if (savedRange != null) {
+        if (window.getSelection)//non IE and there is already a selection
+        {
+            var s = window.getSelection();
+            if (s.rangeCount > 0) 
+                s.removeAllRanges();
+            s.addRange(savedRange);
+        }
+        else if (document.createRange)//non IE and no selection
+        {
+            window.getSelection().addRange(savedRange);
+        }
+        else if (document.selection)//IE
+        {
+            savedRange.select();
+        }
+    }
 }
 
 //редактировать запись
@@ -59,9 +94,50 @@ function onEditNoteClick(index)
 	noteTextArea.contentEditable = "true";
 	
 	/*noteTextArea.addEventListener('keyup', function (event) {
-    var text = this.textContent;
-    var highlighted = text.replace(/(#\w+)/g, '<span class="hashtag">$1</span>');
-    this.innerHTML = highlighted;
+		if(event.keyCode==" ".charCodeAt(0))
+		{
+			console.log("keykod")
+			var root = this;
+			console.log(root)
+			var text = this.textContent;
+			console.log("text"+text)
+			var match = text.match(/(#\w+)/g);
+			console.log("match"+match);
+			if (match!=undefined)
+			for(var i=0;i<match.length;i++)
+			{
+				var foundIndex = text.indexOf(match[i]);
+				console.log("foundIndex="+foundIndex);
+				if(foundIndex!=-1)
+				{
+
+						var rng = document.createRange();
+						var start =  root.getElementsByTagName('span')[i];
+													console.log("start="+start.innerHTML)
+						if(start.innerHTML!=match[i])	
+						{						
+							if((i!=0)&&(start!=undefined))
+							{
+								console.log("start="+start)
+							   rng.setStartAfter(start);
+							   rng.setEnd(start,match[i].length);
+							}
+							else 
+							{
+								rng.setStart( root.firstChild, foundIndex );
+								rng.setEnd( root.firstChild, foundIndex + match[i].length );
+							}
+						console.log("rng="+rng);
+							var highlightDiv = document.createElement('span');
+							highlightDiv.classList.add("hashtag");
+							rng.surroundContents( highlightDiv );
+							//var highlighted = text.replace(/(#\w+)/g, '<span class="hashtag">$1</span>');
+							//this.innerHTML = highlighted;
+							//restoreSelection(noteTextArea);
+						}
+				}
+			}
+		}
     });*/
 
 	var text = noteTextArea.textContent;
@@ -69,21 +145,38 @@ function onEditNoteClick(index)
     var highlighted=text;	
 	for (var i=0;i<allNotes[index].tags.length;i++)
 	{
-	    highlighted = text.replace(new RegExp(allNotes[index].tags[i], 'i'),'<span class="hashtag">'+allNotes[index].tags[i]+'</span>')		
+	    highlighted = text.replace(new RegExp(allNotes[index].tags[i], 'g'),'<span class="hashtag">'+allNotes[index].tags[i]+'</span>')		
 	}
     noteTextArea.innerHTML = highlighted;
     document.getElementById("noteEditDel"+index).style.display = "none";
     document.getElementById("noteOkCancel"+index).style.display = "block";
 	
+	noteTextArea.addEventListener('click', function (event) {
+		/*console.log("onmousedown")
+		if ( window.getSelection )
+		{
+			var s = window.getSelection();
+			console.log("noteTextArea.lastChild="+noteTextArea.lastChild)
+			var rng = document.createRange();
+			rng.setStartBefore(noteTextArea.lastChild);
+			rng.setEndAfter(noteTextArea.lastChild);
+			console.log("rng="+rng.toString());
+			rng.collapse(false);
+			s.removeAllRanges();
+			s.addRange(rng);
+		}*/
+    });
 }
 
 //удаляет из форматирования текста записи теги подсветки ключевых слов
 function redoHighlight(text,index)
-{
-	for (var i=0;i<allNotes[index].tags.length;i++)
-	{
-	    text = text.replace(/<span.*?>.*?<\/span>/ig,allNotes[index].tags[i])		
-	}
+{	
+	text = text.replace(/<span.*?>(.*?)<\/span>/ig, function(a) 
+	{  
+		a=a.replace(/<span.*?>/,'');
+		a=a.replace(/<\/span>/,'');
+		return a;
+	})
 	return text;
 }
 
